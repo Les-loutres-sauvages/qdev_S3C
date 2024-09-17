d793a1f37cbb688d8c131a5a083a1bb3d00e038a
SolareFlame <zeldac332@gmail.com>
2024-09-17 09:47:34 +0200

    new methods on Etudiant and Identite

--- TD2/ex3/src/Etudiant.java
+++ TD2/ex3/src/Etudiant.java
@@ -1,9 +1,51 @@
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Etudiant {
 
     private Identite id;
     private Formation formation;
-    private Map<String, Collection<String>> resultats;
+    private Map<String, Collection<Double>> resultats;
+
+    public Etudiant(Identite id, Formation formation) {
+        this.id = id;
+        this.formation = formation;
+        this.resultats = new HashMap<>();
+    }
+
+    public Etudiant(String nip, String n, String p, Formation f) {
+        this(new Identite(nip, n, p), f);
+    }
+
+    public void addNote(String m, double n) {
+        /*
+        if(!formation.getMatieres().contains(m.toLowerCase())) {
+            throw new IllegalArgumentException("La matière " + m + " n'est pas dans la formation.");
+            return;
+        }
+        */
+
+        if(n < 0 || n > 20) {
+            throw new IllegalArgumentException("La note doit être comprise entre 0 et 20. (actual: " + n + ")");
+            return;
+        }
+
+        resultats.computeIfAbsent(m.toLowerCase(), k -> new ArrayList<>()).add(n);
+        resultats.get(m.toLowerCase()).add(n);
+    }
+
+
+    public Identite getIdentite() {
+        return id;
+    }
+
+    public Formation getFormation() {
+        return formation;
+    }
+
+    public Map<String, Collection<Double>> getResultats() {
+        return resultats;
+    }
 }
--- TD2/ex3/src/Identite.java
+++ TD2/ex3/src/Identite.java
@@ -2,5 +2,28 @@
 
 public class Identite {
 
-    private String nip, nom, prenom;
+    private final String nip, nom, prenom;
+
+    public Identite(String nip, String n, String p) {
+        this.nip = nip;
+        this.nom = n;
+        this.prenom = p;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Identite [nip=" + nip + ", nom=" + nom + ", prenom=" + prenom + "]";
+    }
 }
