9be41f1da79ac808f158f5c925bf7a05727ceab4
SolareFlame <zeldac332@gmail.com>
2024-09-17 09:32:45 +0200

    Initial commit (3.2) last

---
+++ TD2/ex3/src/Etudiant.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+import java.util.Map;
+
+public class Etudiant {
+
+    private Identite id;
+    private Formation formation;
+    private Map<String, Collection<String>> resultats;
+}
--- TD2/ex3/src/Identite.java
+++ TD2/ex3/src/Identite.java
@@ -1,8 +1,6 @@
-import java.util.Collection;
-import java.util.Map;
+
 
 public class Identite {
 
     private String nip, nom, prenom;
-    private Map<String, Collection<String>> notes;
 }
