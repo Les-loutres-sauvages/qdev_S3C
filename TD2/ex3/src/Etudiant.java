01d9889737c30ce2c11ecc1ef3641b3573acfb9a
SolareFlame <zeldac332@gmail.com>
2024-09-17 09:50:11 +0200

    add javadoc

--- TD2/ex3/src/Etudiant.java
+++ TD2/ex3/src/Etudiant.java
@@ -9,27 +9,41 @@ public class Etudiant {
     private Formation formation;
     private Map<String, Collection<Double>> resultats;
 
+    /**
+     * @param id Identité de l'étudiant
+     * @param formation Formation de l'étudiant
+     */
     public Etudiant(Identite id, Formation formation) {
         this.id = id;
         this.formation = formation;
         this.resultats = new HashMap<>();
     }
 
+    /**
+     * @param nip NIP (Numéro d’Identification Personnel)
+     * @param n Nom
+     * @param p Prénom
+     * @param f Formation de l'étudiant
+     */
     public Etudiant(String nip, String n, String p, Formation f) {
         this(new Identite(nip, n, p), f);
     }
 
+    /**
+     * Méthode pour ajouter une note à un étudiant dans une matière donnée
+     *
+     * @param m Matière
+     * @param n Note
+     */
     public void addNote(String m, double n) {
         /*
         if(!formation.getMatieres().contains(m.toLowerCase())) {
             throw new IllegalArgumentException("La matière " + m + " n'est pas dans la formation.");
-            return;
         }
         */
 
         if(n < 0 || n > 20) {
             throw new IllegalArgumentException("La note doit être comprise entre 0 et 20. (actual: " + n + ")");
-            return;
         }
 
         resultats.computeIfAbsent(m.toLowerCase(), k -> new ArrayList<>()).add(n);
--- TD2/ex3/src/Identite.java
+++ TD2/ex3/src/Identite.java
@@ -4,6 +4,11 @@ public class Identite {
 
     private final String nip, nom, prenom;
 
+    /**
+     * @param nip NIP (Numéro d’Identification Personnel)
+     * @param n Nom
+     * @param p Prénom
+     */
     public Identite(String nip, String n, String p) {
         this.nip = nip;
         this.nom = n;
