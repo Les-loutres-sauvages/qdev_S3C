76c875630796a84c4f6dd0be8ecb2efee9d5215e
Valentxn7 <gugug3293@gmail.com>
2024-09-24 09:15:02 +0200

    merge fct_trie + rename GroupeTest_Moyenne.java

--- TD2/ex3/test/GroupeTest_Moyenne.java
+++ TD2/ex3/test/GroupeTest_Moyenne.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class GroupeTest {
+public class GroupeTest_Moyenne {
     Groupe groupe;
     Formation form;
     Etudiant etu1, etu2;
