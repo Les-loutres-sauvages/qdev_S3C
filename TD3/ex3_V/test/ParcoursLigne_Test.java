6953b13cdcef3f1671374ef6f967308783a64392
Valentxn7 <gugug3293@gmail.com>
2024-09-30 11:58:14 +0200

    30/09/24 test fixe Parcours Ligne

--- TD3/ex3_V/test/ParcoursLigne_Test.java
+++ TD3/ex3_V/test/ParcoursLigne_Test.java
@@ -25,14 +25,18 @@ public class ParcoursLigne_Test {
     @Test
     public void test_ajout() {
         ParcoursLigne parcours = tab.iterateurLigne();
+        boolean egal = true;
         int[][] tab2 = new int[tab.getHauteur()][tab.getLargeur()];
 
         while (parcours.hasNext()) {
             int elt = parcours.next();
             System.out.println("elt: " + elt);
             tab2[parcours.getLigneCour()][parcours.getColonneCour()] = elt;
+            if ((tab2[parcours.getLigneCour()][parcours.getColonneCour()]) != (tab.valeurA(parcours.getLigneCour(), parcours.getColonneCour()) )){
+                egal = false;
+            }
         }
-        assertEquals(true, tab.equals(tab2));
+        assertEquals(true, egal);
     }
 
 }
